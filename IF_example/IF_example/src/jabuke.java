3f3674c3db9722516e819edf6fad1dee0139ccd3
lodi432 <glavacevic.d@gmail.com>
2017-12-20 16:30:44 +0100

    Examples

---
+++ IF_example/IF_example/src/jabuke.java
@@ -0,0 +1,12 @@
+public class jabuke {
+public static void main (String args[]) {
+	int test = 6;
+	
+	if (test == 9){
+	System.out.println("Yes");
+	}else{
+		System.out.println("This is Else.");
+	}
+  }
+}
+  
